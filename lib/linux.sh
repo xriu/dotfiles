bea9f6a504b039f8861d2dae3883ea497404802e
Xavier Riu <xavier.riu@launchmetrics.com>
2017-12-25 19:55:45 +0100

    Added docker-compose package

--- lib/linux.sh
+++ lib/linux.sh
@@ -211,6 +211,10 @@ function docker.sh() {
   # Fix docker right
   sudo usermod -aG docker $USER
 
+  # Install docker-compose
+  sudo curl -sL https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+
 }
 
 # Prezto
