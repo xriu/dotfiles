43e7b23f9656fcae2bc1beabf7cb17208982fcd2
Xavier Riu <xavier.riu@launchmetrics.com>
2018-01-17 21:16:40 +0100

    Removed ECS-CLI

--- lib/linux.sh
+++ lib/linux.sh
@@ -163,14 +163,6 @@ function docker() {
 
 }
 
-# ECS-CLI
-function ecs() {
-
-  sudo curl -so /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
-  sudo chmod +x /usr/local/bin/ecs-cli
-
-}
-
 # Prezto
 function prezto() {
 
