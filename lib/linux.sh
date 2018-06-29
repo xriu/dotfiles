7ce9108ab883569703280116cb977cb1e6eb917c
Xavier Riu <xavier.riu@launchmetrics.com>
2018-06-29 22:01:15 +0200

    Update linux.sh

--- lib/linux.sh
+++ lib/linux.sh
@@ -39,6 +39,10 @@ function apt_repositories() {
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
   sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
   sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  
+  # Yarn
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
   # Node 8
   curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
