b382f08cc961a56e70b080307a1b5dbcf9fc9f43
Xavier Riu <xavier.riu@launchmetrics.com>
2019-08-29 21:15:41 +0200

    Added brew terraform

--- lib/osx.sh
+++ lib/osx.sh
@@ -95,8 +95,8 @@ function brew_install() {
     brew install angular-cli
     brew install serverless
     brew install eslint
-    brew install terraform_landscape
-    brew install warrensbox/tap/tfswitch
+    brew install terraform
+    # brew install terraform_landscape
     brew install ansible@1.9
     # sudo /usr/bin/python -m easy_install pip
     # sudo /usr/bin/python -m pip install boto
