607df2fbea3362e295b880d7b4f122076f9f7d96
Xavier Riu <xavier.riu@launchmetrics.com>
2020-07-11 18:19:17 +0200

    Upgrade setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,26 @@ PACKAGE=$1
 
 source ./lib/utils.sh
 
-echo 'Starting packages setup'
-install_packages $PACKAGE
+# Install
+function install() {
+    echo 'Starting packages setup'
+    install_packages $PACKAGE
 
-echo 'Starting common configuration'
-common_configuration
+    echo 'Starting common configuration'
+    common_configuration
+}
+
+# Upgrade
+function upgrade() {
+    echo 'Brew upgrade'
+    brew upgrade
+    brew uninstall perltidy
+    brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/4fb2963d5abee2efa068ed6a0d7a160446f752ef/Formula/perltidy.rb
+}
+
+# Cleanup
+function cleanup() {
+    echo 'Brew cleanup'
+    to_clean=$(brew list -1 | grep -vi openssl | tr '\n' ' ')
+    brew cleanup $to_clean
+}
