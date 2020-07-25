b5c297bce9935439135b4720925eb7ea5164fef6
Xavier Riu <xavier.riu@launchmetrics.com>
2020-07-25 15:51:35 +0200

    Minor improvements

--- setup.sh
+++ setup.sh
@@ -19,15 +19,18 @@ function install {
 function upgrade {
     echo 'Brew upgrade'
     brew upgrade
-    brew uninstall perltidy
-    brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/4fb2963d5abee2efa068ed6a0d7a160446f752ef/Formula/perltidy.rb
+
+    # Perltidy
+    # id='4fb2963d5abee2efa068ed6a0d7a160446f752ef'
+    # brew uninstall perltidy
+    # brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/${id}/Formula/perltidy.rb
 }
 
 # Cleanup
 function cleanup {
     echo 'Brew cleanup'
-    to_clean=$(brew list -1 | grep -vi openssl | tr '\n' ' ')
-    brew cleanup $to_clean
+    for_clean=$(brew list -1 | grep -vi openssl | tr '\n' ' ')
+    brew cleanup $for_clean
 }
 
 # Call function
