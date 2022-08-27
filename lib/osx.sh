5bc187d051b52b32da3969ea24bd24a55f9f1d6c
Xavier Riu <xavier.riu@launchmetrics.com>
2022-08-27 16:56:49 +0200

    Sort packages

--- lib/osx.sh
+++ lib/osx.sh
@@ -33,84 +33,84 @@ function brew_install() {
     brew upgrade
 
     # Desktop Apps
-    brew install --cask temurin --force
-    brew install --cask temurin11 --force
-    brew install --cask docker --force
-    brew install --cask zoom --force
     brew install --cask amazon-chime --force
-    brew install --cask insomnia --force
+    brew install --cask ccleaner --force
+    brew install --cask cyberduck --force
+    brew install --cask dbeaver-community --force
     brew install --cask discord --force
-    brew install --cask session-manager-plugin --force
-    brew install --cask robo-3t --force
-    brew install --cask iterm2 --force
+    brew install --cask docker --force
+    brew install --cask element --force
     brew install --cask google-chrome --force
+    brew install --cask insomnia --force
+    brew install --cask iterm2 --force
     brew install --cask openvpn-connect --force
     brew install --cask rectangle --force
-    brew install --cask element --force
-    brew install --cask spotify --force
+    brew install --cask robo-3t --force
+    brew install --cask session-manager-plugin --force
     brew install --cask slack --force
-    brew install --cask visual-studio-Code --force
-    brew install --cask dbeaver-community --force
-    brew install --cask whatsapp --force
+    brew install --cask spotify --force
     brew install --cask stats --force
-    brew install --cask cyberduck --force
-    brew install --cask ccleaner --force
+    brew install --cask temurin --force
+    brew install --cask temurin11 --force
+    brew install --cask visual-studio-Code --force
+    brew install --cask whatsapp --force
+    brew install --cask zoom --force
 
     # Apps
-    brew install openjdk
-    brew install coreutils
-    brew install moreutils
-    brew install findutils
-    brew install bash
-    brew install bash-completion
-    brew install wget
-    brew install vim
-    brew install nano
-    brew install grep
-    brew install hey
-    brew install openssh
-    brew install openssl
-    brew install screen
-    brew install git
-    brew install svn
-    brew install pv
-    brew install rename
-    brew install nmap
-    brew install leapp
-    brew install Noovolari/brew/leapp-cli
-    brew install termshark
-    brew install fx
-    brew install tree
-    brew install zopfli
-    brew install jenv
-    brew install nvm
     brew install ack
-    brew install dive
-    brew install mkcert
-    brew install helm
-    brew install istioctl
-    brew install ncdu
+    brew install angular-cli
+    brew install autopep8
     brew install awscli
     brew install azure-cli
-    brew install mvn
+    brew install bash
+    brew install bash-completion
+    brew install cdktf
+    brew install coreutils
+    brew install dive
+    brew install docker-slim
+    brew install eslint
+    brew install findutils
+    brew install fx
+    brew install git
     brew install gradle
     brew install gradle-completion
-    brew install htop
-    brew install jq
-    brew install regula
-    brew install docker-slim
-    brew install autopep8
-    brew install openvpn
+    brew install grep
     brew install hadolint
-    brew install angular-cli
-    brew install serverless
-    brew install eslint
+    brew install helm
+    brew install hey
+    brew install htop
+    brew install istioctl
+    brew install jenv
+    brew install jq
+    brew install leapp
+    brew install mkcert
+    brew install moreutils
+    brew install mvn
+    brew install nano
+    brew install ncdu
+    brew install nmap
+    brew install Noovolari/brew/leapp-cli
+    brew install nvm
+    brew install openjdk
+    brew install openssh
+    brew install openssl
+    brew install openvpn
+    brew install pv
     brew install python@3.8
+    brew install regula
+    brew install rename
+    brew install screen
+    brew install serverless
+    brew install svn
+    brew install termshark
+    brew install tree
+    brew install vim
     brew install warrensbox/tap/tfswitch
-    brew install cdktf
+    brew install wget
     brew install yarn
-    brew install zsh
     brew install z
+    brew install zopfli
+    brew install zsh
 
     # Fonts
     brew install font-fira-code
