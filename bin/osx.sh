8fb4a9c6183fb00e4b72015ba3da344e76df9cc7
Xavier Riu <xavier.riu@launchmetrics.com>
2024-04-01 15:56:21 +0200

    Update dependencies in osx.sh script

--- bin/osx.sh
+++ bin/osx.sh
@@ -66,11 +66,8 @@ function brew_setup() {
     brew install --cask warp --force # Rust-based terminal
     brew install --cask zed --force # Multiplayer code editor
 
-    # brew install bandwhich # Terminal bandwidth utilization tool
-    # brew install trippy # A network diagnostic tool
-    # brew install openjdk # Development kit for the Java programming language
     # brew install openssl # Cryptography and SSL/TLS Toolkit
-    # brew install protobuf # Protocol buffers (Google's data interchange format)
+    brew install protobuf # Protocol buffers (Google's data interchange format)
     brew install angular-cli # CLI tool for Angular
     brew install atuin # Magical shell history
     brew install awscli # Official Amazon AWS command-line interface
