0855250b52740bedea5d4edb9c934a2770b5437e
Xavier Riu <xavier.riu@launchmetrics.com>
2024-06-23 20:26:00 +0200

    chore: Update macOS setup script to include Git Butler installation

--- bin/osx.sh
+++ bin/osx.sh
@@ -40,6 +40,7 @@ function brew_setup() {
     # Install Rosetta
     softwareupdate --install-rosetta --agree-to-license
 
+    # brew install --cask gitbutler --force # Git client
     # brew install --cask zoom --force # Video communication and virtual meeting platform
     brew install --cask 1password --force # Password manager that keeps all passwords secure behind one password
     brew install --cask amazon-chime --force # Unified communications service that transforms online meetings with a secure, easy-to-use application
