9cb0ea9f476b715e793db0bf324ad35c7649f8bf
Xavier Riu <xavier.riu@launchmetrics.com>
2025-01-08 09:32:52 +0100

    chore(aliases.zsh): simplify ls and ll aliases by removing old commented definitions

--- zsh/functions/aliases.zsh
+++ zsh/functions/aliases.zsh
@@ -16,8 +16,6 @@ alias egrep="egrep --color=auto"
 alias cat="bat"
 
 # List
-# alias ls="gls --color"
-# alias ll="ls -alh --group-directories-first"
 alias ls="lla"
 alias ll="ls -l --sort-dirs-first"
 
