be4b4ba6a296dbeca635749f4496817883c7250c
Xavier Riu <xavier.riu@launchmetrics.com>
2025-05-11 10:58:17 +0200

    Remove package

--- bin/osx.sh
+++ bin/osx.sh
@@ -41,7 +41,6 @@ function brew_setup() {
     # Install Rosetta
     softwareupdate --install-rosetta --agree-to-license
 
-    # brew install --cask onyx --force # Multifunction utility for macOS
     # brew install --cask zoom --force # Video communication and virtual meeting platform
     brew install --cask 1password --force # Password manager that keeps all passwords secure behind one password
     brew install --cask amazon-chime --force # Unified communications service that transforms online meetings with a secure, easy-to-use application
