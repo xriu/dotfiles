9bc7e4bbe28055db1dbb04338c96957c1b361840
Xavier Riu <xavier.riu@launchmetrics.com>
2025-10-30 12:04:57 +0100

    Replace chatgpt cask with chatgpt-atlas

--- bin/osx.sh
+++ bin/osx.sh
@@ -48,7 +48,7 @@ function brew_setup() {
     # brew install --cask zoom --force # Video communication and virtual meeting platform
     brew install --cask 1password --force # Password manager that keeps all passwords secure behind one password
     brew install --cask appcleaner --force # Application uninstaller
-    brew install --cask chatgpt --force # Chat with an AI that can generate human-like responses
+    brew install --cask chatgpt-atlas --force # ChatGPT client for macOS
     brew install --cask cursor --force # Write, edit, and chat about your code with AI
     brew install --cask cyberduck --force # Server and cloud storage browser
     brew install --cask dbeaver-community --force # Universal database tool and SQL client
