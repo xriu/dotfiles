cb9f55149bb8774dfd4c8f9b28cdbfe740498ef7
Xavier Riu <xavier.riu@launchmetrics.com>
2025-12-14 16:14:02 +0100

    Enhance branch deletion in gbd function by allowing pattern matching. The function now lists branches matching the provided patterns before deletion, improving usability and flexibility.

--- zsh/functions/git.zsh
+++ zsh/functions/git.zsh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 
 gbd() {
-  for branch in "$@"; do
-    git branch -D "$branch"
+  for pattern in "$@"; do
+    git branch --list "$pattern" | tr -d ' *' | while read -r branch; do
+      [[ -n "$branch" ]] && git branch -D "$branch"
+    done
   done
 }
