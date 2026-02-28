6c2fa6aee43ad480518b96f552f8de091f61d25b
Xavier Riu <xavier.riu@launchmetrics.com>
2026-02-28 19:11:30 +0100

    Reorganize zsh pane renaming functions for clarity and consistency by moving the rename_pane_to_cmd function below the rename_pane_to_cwd function and ensuring both are registered with appropriate hooks.

--- zsh/functions/zellij.zsh
+++ zsh/functions/zellij.zsh
@@ -1,17 +1,17 @@
-# Rename pane to the command being run
-rename_pane_to_cmd() {
-    [[ -z "$ZELLIJ" || -z "$1" ]] && return
-    zellij action rename-pane "${1%% *}" &>/dev/null &!
-}
-
 # Reset pane name to current directory when idle
 rename_pane_to_cwd() {
     [[ -z "$ZELLIJ" ]] && return
     zellij action rename-pane "${PWD:t}" &>/dev/null &!
 }
 
+# Rename pane to the command being run
+rename_pane_to_cmd() {
+    [[ -z "$ZELLIJ" || -z "$1" ]] && return
+    zellij action rename-pane "${1%% *}" &>/dev/null &!
+}
+
 # Register hooks (function names are arbitrary)
 autoload -Uz add-zsh-hook
-add-zsh-hook preexec rename_pane_to_cmd
 add-zsh-hook precmd rename_pane_to_cwd
+add-zsh-hook preexec rename_pane_to_cmd
 
