b4f13bfea0de003582fecfce37d56bc5686e570e
Xavier Riu <xavier.riu@launchmetrics.com>
2026-03-09 22:44:19 +0100

    Add initial Fish configuration with greeting disable and OrbStack integration

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+# # Disable greeting
+# set fish_greeting
+
+# # Added by OrbStack: command-line tools and integration
+# # This won't be added again if you remove it.
+# source ~/.orbstack/shell/init2.fish 2>/dev/null || :
