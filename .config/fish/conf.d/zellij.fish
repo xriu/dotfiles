7f90b27c9f069d9dfc17deb1ae89404a26b77b04
Xavier Riu <xavier.riu@launchmetrics.com>
2026-03-11 10:38:56 +0100

    feat: Enhance Zellij auto-start configuration and set default shell to Fish

--- .config/fish/conf.d/zellij.fish
+++ .config/fish/conf.d/zellij.fish
@@ -5,7 +5,5 @@ set -q ZELLIJ; and return
 set -e GHOSTTY_SHELL_INTEGRATION_NO_TITLE
 
 if set -q GHOSTTY_QUICK_TERMINAL
-    sleep 1; and zellij -l ~/.config/zellij/layouts/quick.kdl attach quick -c
-else
-    sleep 1;
+    zellij -l ~/.config/zellij/layouts/quick.kdl attach quick -c
 end
