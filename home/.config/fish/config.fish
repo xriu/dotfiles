f3656def7c569494fced2cbe1f6eb7ad86f3693a
Xavier Riu <xavier.riu@launchmetrics.com>
2026-09-17 22:44:45 +0200

    chore(fish): swap ALIBABA_API_KEY for TYPESAFE_JUDGMENTS in launchctl sync

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -45,7 +45,7 @@ if status is-interactive
 
     # Sync secrets to the macOS GUI session so GUI apps (e.g. bb) inherit them.
     # Values come from config.local.fish sourced above. Runs in interactive shells.
-    launchctl setenv ALIBABA_API_KEY $ALIBABA_API_KEY
+    launchctl setenv TYPESAFE_JUDGMENTS 1
     launchctl setenv OPENROUTER_API_KEY $OPENROUTER_API_KEY
     launchctl setenv OPENCODE_API_KEY $OPENCODE_API_KEY
 
